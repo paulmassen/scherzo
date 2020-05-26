7ba6632d515339b29322c2b9ebdefc858433514d
Paul Massendari <massendari.paul@gmail.com>
2020-05-26 10:56:26 +0000

    redesign in progress

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+
+//= require vertical-menu-light.js
+
+//= require app-menu.js 
+//= require components.js
+//= require footer.js
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,54 +3,62 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//require("stimulus-inline-edit");
 require("@rails/ujs").start();
 require("turbolinks").start();
-//require("stimulus_reflex");
 require("@rails/activestorage").start();
 require("channels");
+
 require("jquery");
+
 require("src/cocoon");
 require("moment");
 require("fullcalendar");
 require("trix");
 require("@rails/actiontext");
-//require("selectize");
 require("choices.js");
-//require("stimulus-inline-edit");
-//require("jquery-ui");
-//require("materialize-css")
-// jquery
-//import Choices from 'choices.js';
-import { Controller } from 'stimulus'
-import StimulusReflex from 'stimulus_reflex'
+require("src/vendors.min.js");
+
+
+import "controllers";
+import { Controller } from 'stimulus';
+import StimulusReflex from 'stimulus_reflex';
 import $ from 'jquery';
 import selectize from 'selectize';
 import * as Choices from 'choices.js'
-global.$ = $
-global.jQuery = $
+global.$ = $;
+global.jQuery = $;
 
+// ============================================
+
+
+// ============================================
  $(document).on('turbolinks:load', function() {
  
-
+  $('#calendar').fullCalendar({
+  //var calendar = $(this);
+	schedulerLicenseKey : 'GPL-My-Project-Is-Open-Source',
+	editable : true,
+	selectable : true,
+	selectHelper : true,
+	header: {
+	  left: 'prev,next today',
+	  center: 'title',
+	  right: 'month, agendaWeek, agendaDay'
+	},
+	scrollTime : '00:00',
+	defaultDate : '2020-04-02',
+	events : '/engagements.json',
+	select: function(start, end){
+	  $.getScript('/engagements/new', function(){
+	    
+	  });
+	  //calendar.fullCalendar('unselect')
+	}
 
   });  
 
 
-//require("materialize");
-//require("material-icons");
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-
-//import 'formSelect';
-//import 'sidenav';
-//import "controllers";
-import 'materialize-css/dist/js/materialize.js';
 /*function eventCalendar() {
   return $('#calendar').fullCalendar({ });
 };
@@ -61,74 +69,10 @@ import 'materialize-css/dist/js/materialize.js';
   $('#calendar').html('');
 };*/
 
-/*    const Choices = require('choices.js');
-  var singleFetch = new Choices('#mydemo', {
-          searchPlaceholderValue: 'Search a structure',
-          removeItemButton: true,
-        })
-          .setChoices(function() {
-            return fetch(
-              '/structures.json'
-            )
-              .then(function(response) {
-                return response.json();
-              })
-              .then(function(data) {
-                return data.map(function(release) {
-                  return { label: release.title, value: release.id };
-                });
-              });
-          });
-          
-})*/
 
-$(document).on('turbolinks:load', function() {
-    $('.collapsible').collapsible({
-    accordion: false
-  });
- /* const Choices = require('choices.js');
-  var singleFetch = new Choices('#mydemo', {
-          searchPlaceholderValue: 'Search a structure',
-          removeItemButton: true,
-        })
-          .setChoices(function() {
-            return fetch(
-              '/structures.json'
-            )
-              .then(function(response) {
-                return response.json();
-              })
-              .then(function(data) {
-                return data.map(function(release) {
-                  return { label: release.title, value: release.id };
-                });
-              });
-          });*/
-          
-  $('.composer-autocomplete').autocomplete({
-    data: {
-      "Mozart": 'Mozart',
-      "Beethoven": 'Beethoven',
-      "Haydn": 'Haydn'
-    },
-  });
+
 
 $('#remove_structure').click(function(event){
   event.preventDefault();
 });
-
-
-  $('select:not(#mydemo)').formSelect();
-  //$('.datepicker').datepicker();
-  $('.sidenav').sidenav();
-
-  /*    $(document).on('turbolinks:load', function(){
-    eventCalendar();  
-  });*/
-
-
-  /*$(document).on('turbolinks:before-cache', clearCalendar);*/
-
 });
-
-import "controllers"
